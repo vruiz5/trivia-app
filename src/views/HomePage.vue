2d381edaaed74a5b23a8bf64e667509a5d54b98b
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-03-03 18:31:24 -0600

    API call working

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,19 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import BaseTitle from '@/components/BaseTitle.vue'
+import useAPI from '@/composables/useApi'
+
+const { categories, getCategories } = useAPI()
+
+onMounted(async () => {
+  await getCategories();
+})
+
 </script>
 
 <template>
   <BaseTitle>TRIVIA APPLICATION</BaseTitle>
+    <div v-for="category in categories" :key="category.id">
+      {{ category.name }}
+    </div>
 </template>

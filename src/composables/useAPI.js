d1ed54d1e25edb0c2e8d805c1608b877eb3b1e64
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-03-10 19:25:00 -0500

    GetQuestion is working

--- src/composables/useAPI.js
+++ src/composables/useAPI.js
@@ -2,18 +2,29 @@ import axios from 'axios';
 import { ref } from 'vue';
 
 const instance = axios.create({
-  baseURL: 'https://opentdb.com/',
-});
+    baseURL: 'https://opentdb.com/',
+})
 
 const categories = ref([]);
 
 export default function useAPI() {
-  const getCategories = async () => {
-    if (categories.value.length === 0) {
-      const response = await instance.get('api_category.php')
-      categories.value = response.data.trivia_categories;
+    const getCategories = async () => {
+      if (categories.value.length === 0) {
+        const response = await instance.get('api_category.php')
+        categories.value = response.data.trivia_categories;
+      }
     }
-  };
 
-  return { categories, getCategories }
+    const getQuestion = async (categoryID) => {
+      const response = await instance.get('api.php', {
+        params: {
+          amount: 1,
+          category: categoryID,
+        }
+      })
+
+    return response.data.results[0]
+  }
+
+  return { instance, categories, getCategories, getQuestion }
 }

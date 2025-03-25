9e8d16460631afb09548b81fddc55a6c3060c48a
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-03-25 17:44:13 -0500

    Added Scoring to Questions Page

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -5,11 +5,17 @@ import { useRoute } from 'vue-router'
 import BaseTitle from '@/components/BaseTitle.vue'
 import DifficultyChip from '@/components/DifficultyChip.vue'
 import MainScore from '@/components/MainScore.vue'
+import useScore from '@/composables/useScore'
+import router from '@/router'
 
 const api = useAPI()
 const question = ref(null)
 const route = useRoute()
 const answers = ref([])
+const {changeScore} = useScore()
+const notification = ref('')
+
+
 
 onMounted(async () => {
   question.value = await api.getQuestion(route.params.id)
@@ -17,14 +23,16 @@ onMounted(async () => {
   answers.value.push({
     id: answers.value.length,
     correct: true,
-    answer: question.value.correct_answer
+    answer: question.value.correct_answer,
+    points: question.value.difficulty === 'easy' ? 10 : question.value.difficulty === 'medium' ? 20 : 30
   })
 
   question.value.incorrect_answers.map((wrong_answer) => {
     answers.value.push({
       id: answers.value.length,
       correct: false,
-      answer: wrong_answer
+      answer: wrong_answer,
+      points: -5,
     })
 
   })
@@ -44,21 +52,43 @@ const shuffle = (array) => {
   return array
 }
 
+// Handle answer choices and award points
+const handleAnswer = (points) => {
+  changeScore(points)
+
+  if (points > 0) {
+    notification.value = 'Correct!'
+  }
+  else {
+    notification.value = 'Incorrect!'
+  }
+
+  setTimeout(() => {
+    router.push('/trivia-app/')
+  }, 1000)
+}
+
 </script>
 
 
 
-
 <template>
+
   <div v-if="question" class="flex h-full w-full flex-col items-center gap-8 p-10">
-    <BaseTitle>{{ question.category }} - <MainScore></MainScore></BaseTitle>
+    <BaseTitle>
+      <MainScore></MainScore> &nbsp;
+      <span class="font-bold" :class="notification === 'Correct!' ? 'text-green-500' : 'text-red-500'">
+        {{ notification }}
+      </span>
+    </BaseTitle> 
     <!-- {{ question.question }} -->
 
     <div v-html="question.question" class="text-cent text-2xl font-bold"></div>
     <div class="grid w-full flex-grow grid-cols-2 gap-8">
         <div v-for="answer in answers" 
           v-html="answer.answer" 
-          :key="answer.id" 
+          :key="answer.id"
+          @click="handleAnswer(answer.points)"
           class="bg-green-500 flex items-ceter justify-center text-4xl rounded-lg text-white py-10 px-2">
         </div>
     </div>
